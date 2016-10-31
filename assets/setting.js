85cb24dfa063dbc81f224d83f8e7aa207a4fafbf
Vu Nguyen <Vu Nguyen>
2016-10-31 09:59:29 +0700

    update

---
+++ assets/setting.js
@@ -0,0 +1,72 @@
+// ============= HOME ==============
+var Page = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Openmize ",
+    subHeading: "Openmize | Bóng đèn đuổi muỗi chất lượng cao",
+    mainHeading: "OpenMize là nhà cung cấp bóng đèn đuổi muỗi chất lượng cao cho mọi gia đình",
+    CTA: "Xem sản phẩm của chúng tôi",
+    brief: "Sản phẩm bóng đèn đuổi muỗi của chúng tôi xuất khẩu sang châu Âu và được nhiều thị trường khó tính chấp nhận: Đức, Pháp, Úc, Mỹ…",
+
+    // ============= VARS ==============
+    url : "http://www.openmize.top/", //-- co slash cuoi /
+    mainkeyWord : "bóng đèn đuổi muổi",
+    longkeyWords : ["bóng đèn đuổi muỗi","muỗi zika"],
+    facebookAppId : "303370230021409", //-- your appID
+    googleAnalyticId : "UA-82197343-2", //-- your id
+    formId: "https://docs.google.com/forms/d/e/1FAIpQLScjBXL-M0eRB0qhX1xSV5sWWAwsXbNqtjAGm3aq0-l5EZAisg/viewform?embedded=true",
+}
+
+var Social = {
+    //-- de trong "" de giu nguyen default text
+    email: "maito:haitd@openmize.com",
+    facebook: "1",
+    twitter: "2",
+    google: "3",
+    instagram: "3",
+    pinterest: "0",
+    linkedin: "0"
+}
+
+var colors = [
+    "red","blue","green","orange",  // 0 1 2 3  
+    "pink", "purple", "light-blue", "cyan", // 4 5 6 7  
+    "deep-purple", "indigo", "teal", "lime", // 8 9 10 11
+    "light-green", "yellow", "amber", "deep-orange", // 12 13 14 15
+    "brown", "grey", "blue-grey"      // 16 17 18 19
+];
+
+colors = ['red']
+var i = Math.floor( Math.random() * (colors.length - 0) + 0 );
+var chooseColor = colors[i].toString();
+
+
+// ============= PERSONAL ==============
+var Person = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "OpenMize",
+    fullname: "<strong>OpenMize</strong>.",
+    slogan: "Unique. Sports. Professionals",
+    sex: '<i class="fa fa-male"></i>', //-- male, female
+    phone: 'tel:0937980003',
+    // message: "Tôi đã sẵn sàng, còn bạn thì sao?", //-- thong diep chinh hien nay!
+    message: "Bóng đèn đuổi muỗi chất lượng cao",
+    
+    facebook: 'https://facebook.com/nguyencongvu',
+    twitter: "2",
+    google: "3",
+    instagram: "3",
+    pinterest: "0",
+    linkedin: "0",
+    google: "g",
+    CTA: "Tôi muốn có ngay Landing Page",
+
+}
+
+
+var Social = {
+
+}
+
+
+
+    
